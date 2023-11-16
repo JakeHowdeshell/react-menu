fc5a5013d795152bcdcdbdae2acca0f648aa3add
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-11-15 20:03:03 -0600

    Added server code and created typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,67 @@
 const typeDefs = `
-  
+type Category {
+    _id: ID
+    name: String
+  }
+
+  type Meal {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Meal]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    phoneNumber: String
+    password: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  input MealInput {
+    _id: ID
+    purchaseQuantity: Int
+    name: String
+    image: String
+    price: Float
+  }
+
+  type Query {
+    categories: [Category]
+    meals(category: ID, name: String): [Meal]
+    meal(_id: ID!): Meal
+    user: User
+    order(_id: ID!): Order
+    checkout(meals: [MealInput]): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, phoneNumber: String!, password: String!): Auth
+    addOrder(meals: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, phoneNumber: String, password: String): User
+    updateMeal(_id: ID!, quantity: Int!): Meal
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
