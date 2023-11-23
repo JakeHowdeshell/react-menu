56b9f9d40aa98e40ef8121c66e28827e0d3354a4
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-11-22 22:04:37 -0600

    make the orderhistory to redetch data after checkout

--- client/src/pages/OrderHistory.jsx
+++ client/src/pages/OrderHistory.jsx
@@ -1,22 +1,23 @@
-import { Link } from 'react-router-dom';
-
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import { QUERY_USER } from "../utils/queries";
 
 function OrderHistory() {
-  const { data } = useQuery(QUERY_USER);
-  let user;
+  const { data, loading, error } = useQuery(QUERY_USER);
+  const [user, setUser] = useState({});
 
-  if (data) {
-    user = data.user;
-  }
+  useEffect(() => {
+    if (loading) return;
+    setUser(data.user);
+  }, [data]);
 
-  return (
+  return (data) ? (
     <>
       <div className="container my-1">
         <Link to="/">← Back to Menu</Link>
 
-        {user ? (
+        {user.orders ? (
           <>
             <h2>
               Order History for {user.firstName} {user.lastName}
@@ -26,11 +27,18 @@ function OrderHistory() {
                 <h3>
                   {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
                 </h3>
-                <div className="d-flex justify-content-md-center align-content-between flex-wrap" style={{ textAlign: "center" }}>
+                <div
+                  className="d-flex justify-content-md-center align-content-between flex-wrap"
+                  style={{ textAlign: "center" }}
+                >
                   {order.meals.map(({ _id, image, name, price }, index) => (
                     <div key={index} className="card p-1">
-                      <Link to={`/meals/${_id}`} >
-                        <img alt={name} src={`/images/${image}`} className="card-image" />
+                      <Link to={`/meals/${_id}`}>
+                        <img
+                          alt={name}
+                          src={`/images/${image}`}
+                          className="card-image"
+                        />
                         <p>{name}</p>
                       </Link>
                       <div>
@@ -45,6 +53,8 @@ function OrderHistory() {
         ) : null}
       </div>
     </>
+  ) : (
+    <h1>loading</h1>
   );
 }
 
