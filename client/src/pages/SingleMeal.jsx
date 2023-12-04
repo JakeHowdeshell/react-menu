bb2eaabe4f6dff37450bcd2f104868fba4214176
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-12-04 15:22:20 -0600

    added the toast imports as well as the message

--- client/src/pages/SingleMeal.jsx
+++ client/src/pages/SingleMeal.jsx
@@ -6,66 +6,72 @@ import { useStoreContext } from "../utils/GlobalState";
 import { useParams, Link } from "react-router-dom";
 import { idbPromise } from "../utils/helpers";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function SingleMeal() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const {loading, data} = useQuery(QUERY_MEAL, {
-        variables: {id: id},
-    });
-    const meal = data?.meal || {};
+  const { loading, data } = useQuery(QUERY_MEAL, {
+    variables: { id: id },
+  });
+  const meal = data?.meal || {};
 
-    console.log("meal: ", meal);
-    const [state, dispatch] = useStoreContext();
+  console.log("meal: ", meal);
+  const [state, dispatch] = useStoreContext();
 
-    const { image, name, _id, price, description } = meal;
-    console.log(data);
-  
-    const { cart } = state;
-  
-    const addToCart = () => {
-      const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-      if (itemInCart) {
-        dispatch({
-          type: UPDATE_CART_QUANTITY,
-          _id: _id,
-          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-        });
-        idbPromise("cart", "put", {
-          ...itemInCart,
-          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-        });
-      } else {
-        dispatch({
-          type: ADD_TO_CART,
-          meal: { ...meal, purchaseQuantity: 1 },
-        });
-         idbPromise("cart", "put", { ...meal, purchaseQuantity: 1 });
-      }
-      alert("Meal added to Cart!");
-    };
+  const { image, name, _id, price, description } = meal;
+  console.log(data);
 
-    if(loading) {
-        return <div>Loading...</div>
+  const { cart } = state;
+
+  const addToCart = () => {
+    toast("Your meal was added to the cart!");
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+      idbPromise("cart", "put", {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        meal: { ...meal, purchaseQuantity: 1 },
+      });
+      idbPromise("cart", "put", { ...meal, purchaseQuantity: 1 });
     }
+    // alert("Meal added to Cart!");
+  };
 
-    return (
-      <div className="singleMeal">
-        <div className="col d-flex justify-content-center flex-wrap mb-5 p-3">
-          <div className="card">
-            <h2 className="singleMealTitle">{name}</h2>
-            <img src={`/images/${image}`} className="card-image"></img>
-            <div className="singleMealDesc">{description}</div>
-              <h2 className="cardActions2">$ {price}</h2>
-            <div className="cardActions2">
-              <button className="comic-button-singleMeal" onClick={addToCart}>
-                Add to Cart
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="singleMeal">
+      <div className="col d-flex justify-content-center flex-wrap mb-5 p-3">
+        <div className="card">
+          <h2 className="singleMealTitle">{name}</h2>
+          <img src={`/images/${image}`} className="card-image"></img>
+          <div className="singleMealDesc">{description}</div>
+          <h2 className="cardActions2">$ {price}</h2>
+          <div className="cardActions2">
+            <button className="comic-button-singleMeal" onClick={addToCart}>
+              Add to Cart
+            </button>
+            <Link to="/">
+              <button className="comic-button-singleMeal">
+                Return to full menu
               </button>
-              <Link to="/">
-                <button className="comic-button-singleMeal">Return to full menu</button>
-              </Link>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
